029a7369659bc98e1a393a0e2beb362848e4262b
chakravarthi000087-byte <chakravarthi000087@gmail.com>
2026-09-11 12:19:33 +0530

    fibnocci_series.py

--- unit1/electicitybill.py
+++ unit1/electicitybill.py
@@ -1,20 +1,17 @@
-name = str(input('enter the name of the customer : '))
-customer_ID = str(input('enter the customer id : '))
-previous_meter_reading = float(input('enter the previous meter reading : '))
-current_meter_reading = float(input('enter the current meter reading : '))
-if current_meter_reading > previous_meter_reading:
-    total_units_consumed = current_meter_reading - previous_meter_reading
-    cost_per_unit = float(input('enter the cost per unit : '))
-    energy_charge = total_units_consumed * cost_per_unit
-    electrical_duty = 0.05 * energy_charge
-    fixed_meter_charge = 100
-    net_bill = energy_charge + electrical_duty + fixed_meter_charge
-    print(f'name of the customer is {name}')
-    print(f'customer id is {customer_ID}')
-    print(f'total units consumed are {total_units_consumed}')
-    print(f'energy charge is {energy_charge}')
-    print(f'electrical duty is {electrical_duty}')
-    print(f'fixed meter charge is {fixed_meter_charge}')
-    print(f'net bill is {net_bill}')
-else :
-    print('error: current meter reading should be greater than previous meter reading')
+#Like I will send the electricity bill generater
+consumer_name=input("Enter your name:")
+consumer_Id=float(input("Enter your ID:"))
+Previous_Meter_Reading=float(input("Enter your previous meter_reading:"))
+Current_Meter_Reading=float(input("Enter your current meter_reading:"))
+cost_per_unit=float(input("Enter your cost per unit:"))
+units=float(input("Enter your units:"))
+units_consumed=Current_Meter_Reading-Previous_Meter_Reading
+Energy_charge=cost_per_unit*units
+Electricity_Duty=5% Energy_charge
+Fixed_meter_charge=100
+Net_bill=Energy_charge+Electricity_Duty+Fixed_meter_charge
+print("units consumed:",units_consumed)
+print("Energy_charge:",Energy_charge)
+print("Electricity duty:",Electricity_Duty)
+print("Fixed meter charge:",Fixed_meter_charge)
+print("Net bill:",Net_bill)
